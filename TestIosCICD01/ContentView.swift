fb0db2edaf86af5bc0a00112bbd3e87a95b632af
Habibur Rahman <habibur.rahman@dreamonline.co.jp>
2024-02-05 16:26:02 +0600

    Add Crash

--- TestIosCICD01/ContentView.swift
+++ TestIosCICD01/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button{
-                Crashes.generateTestCrash()
+                
+                //Crashes.generateTestCrash()
             }label: {
                 Text("Click Me")
             }
