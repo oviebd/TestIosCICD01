69ff0e84a30cbfca6697e13ff0edd8e0a8082ae5
Habibur Rahman <habibur.rahman@dreamonline.co.jp>
2024-02-05 15:41:58 +0600

    Add Project . Initial Commit

---
+++ TestIosCICD01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestIosCICD01
+//
+//  Created by Habibur Rahman on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestIosCICD01/TestIosCICD01App.swift
@@ -0,0 +1,17 @@
+//
+//  TestIosCICD01App.swift
+//  TestIosCICD01
+//
+//  Created by Habibur Rahman on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TestIosCICD01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
